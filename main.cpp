fee4315285a16a54ef14173b814435f1388c1b10
Raaschou <168561326+Raaschou@users.noreply.github.com>
2026-02-15 17:49:41 +0100

    Add render window with line

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <list>
 #include "mergeSort.h"
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
 
 int main() {
     std::cout << "Unsorted array:" << std::endl;
     std::list<int> toSort = {11, 21, 3, 54, 76, 43, 67, 64, 54, 100 };
-    for (int i : toSort) {
-        std::cout << "[" << i << "]-";
+
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Algo visualisation");
+
+    sf::RectangleShape line({150.f, 5.f});
+    line.rotate(sf::degrees(90));
+    sf::Vector2f vectorTranslate {100.f, 100.f};
+    line.setPosition(vectorTranslate);
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+        window.draw(line);
+
+        window.display();
     }
+
+
     std::cout << std::endl << "Sorted array" << std::endl;
     std::list<int> sorted {mergeSort(toSort)};
     for (int i : sorted) {
