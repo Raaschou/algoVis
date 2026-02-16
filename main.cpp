d8e6bae12f38e6d3e79981a19a4fa0949c80a7bc
Raaschou <168561326+Raaschou@users.noreply.github.com>
2026-02-16 12:23:24 +0100

    Fail to render list as bars

--- main.cpp
+++ main.cpp
@@ -6,14 +6,21 @@
 
 int main() {
     std::cout << "Unsorted array:" << std::endl;
-    std::list<int> toSort = {11, 21, 3, 54, 76, 43, 67, 64, 54, 100 };
-
+    std::list<float> toSort = {1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f };
+    for (float i : toSort) {
+        std::cout << "[" << i << "]-";
+    }
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Algo visualisation");
+    std::vector<sf::RectangleShape> lines {};
+    float lineHeight {150.f};
+    for (float i : toSort) {
+        lines.push_back(sf::RectangleShape({lineHeight, 10.f}));
+        lineHeight += 50.f;
+    }
 
-    sf::RectangleShape line({150.f, 5.f});
-    line.rotate(sf::degrees(90));
+    //line.rotate(sf::degrees(90));
     sf::Vector2f vectorTranslate {100.f, 100.f};
-    line.setPosition(vectorTranslate);
+    //line.setPosition(vectorTranslate);
     window.setFramerateLimit(60);
 
     while (window.isOpen()) {
@@ -24,16 +31,19 @@ int main() {
         }
 
         window.clear(sf::Color::Black);
+        for (sf::RectangleShape line : lines) {
+            line.rotate(sf::degrees(90));
+            window.draw(line);
+        }
 
-        window.draw(line);
 
         window.display();
     }
 
 
     std::cout << std::endl << "Sorted array" << std::endl;
-    std::list<int> sorted {mergeSort(toSort)};
-    for (int i : sorted) {
+    std::list<float> sorted {mergeSort<float>(toSort)};
+    for (float i : sorted) {
         std::cout << "[" << i << "]-";
     }
 }
